f8de6d8fe6c27a34bb5d2ac20a0fd4b017907d3b
LPGabriel <gabrielvinilima2@gmail.com>
2022-12-02 00:19:58 -0300

    Estruta da pagina home e navegação

--- src/Pages/AppExplanation/index.jsx
+++ src/Pages/AppExplanation/index.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 import DefaultButton from "../../Components/Common/DefaultButton";
 import ExplanationCard from "../../Components/Explanation/ExplanationCard";
 
 export default function AppExplanation() {
 
-	function handleSetShowHome() {
-    console.log("Botão clicado!");
+  const navigation = useNavigation();
+
+  function handleNavHome() {
+    navigation.navigate("Home");
   }
 
   return (
@@ -32,7 +35,7 @@ export default function AppExplanation() {
           
           <DefaultButton
             buttonText={"Continuar"}
-            handlePress={handleSetShowHome}
+            handlePress={handleNavHome}
             width={250}
             height={50}
           />
