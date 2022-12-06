199dab5bf95debb230ea7b06d8e7730963681ed8
LPGabriel <gabrielvinilima2@gmail.com>
2022-12-06 15:12:59 -0300

    Estrutura da StatusBar e botão de navegação (ver explicação)

--- src/Pages/AppExplanation/index.jsx
+++ src/Pages/AppExplanation/index.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { View, Text, StyleSheet, ScrollView } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-
+import {View, Text, StyleSheet, ScrollView, handleNavHome} from "react-native";
 import DefaultButton from "../../Components/Common/DefaultButton";
 import ExplanationCard from "../../Components/Explanation/ExplanationCard";
+import { useNavigation } from "@react-navigation/native";
 
 export default function AppExplanation() {
-
   const navigation = useNavigation();
 
   function handleNavHome() {
@@ -16,30 +14,24 @@ export default function AppExplanation() {
   return (
     <View style={styles.container}>
       <ScrollView>
-        
         <View style={{ alignItems: "center" }}>
           <Text style={styles.title}>
             Antes, deixa {"\n"} eu te explicar...
           </Text>
-
           <ExplanationCard />
-          
           <Text style={styles.descriptionCta}>
             Pronto(a) para subir de nível na vida?
           </Text>
-          
           <Text style={styles.description}>
             Na próxima tela você vai poder escolher {"\n"} seus 4 hábitos de
             forma individual.
           </Text>
-          
           <DefaultButton
             buttonText={"Continuar"}
             handlePress={handleNavHome}
             width={250}
             height={50}
           />
-        
         </View>
       </ScrollView>
     </View>
