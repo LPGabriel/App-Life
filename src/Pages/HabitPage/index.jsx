c1ad97f34bddf9c47c91387ed3ab7769c527f8f4
LPGabriel <gabrielvinilima2@gmail.com>
2022-12-13 17:38:11 -0300

    Linkando a página com o serviço (CreateHabit)

--- src/Pages/HabitPage/index.jsx
+++ src/Pages/HabitPage/index.jsx
@@ -15,6 +15,7 @@ import Notification from "../../Components/Common/HabitPage/Notification";
 import TimeDatePicker from "../../Components/Common/HabitPage/TimeDataPicker";
 import UpdateExcludeButtons from "../../Components/Common/HabitPage/UpdateExcludeButtons";
 import DefaultButton from "../../Components/Common/DefaultButton";
+import HabitsService from "../../Service/HabitService";
 
 export default function HabitPage({ route }) {
   const navigation = useNavigation();
@@ -25,6 +26,11 @@ export default function HabitPage({ route }) {
   const [timeNotification, setTimeNotification] = useState();
 
   const { create, habit } = route.params;
+
+  const habitCreated = new Date();
+  const formatDate = `${habitCreated.getFullYear()}-${
+    habitCreated.getMonth() + 1
+  }-${habitCreated.getDate()}`;
 
   function handleCreateHabit() {
     if (habitInput === undefined || frequencyInput === undefined) {
@@ -57,8 +63,23 @@ export default function HabitPage({ route }) {
     if (notificationToggle === true && !dayNotification && !timeNotification) {
       Alert.alert("Você precisa colocar a frequência e horário da notificação");
     } else {
-      navigation.navigate("Home", {
-        updatedHabit: `Updated in ${habit?.habitArea}`,
+      HabitsService.createHabit({
+        habitArea: habit?.habitArea,
+        habitName: habitInput,
+        habitFrequency: frequencyInput,
+        habitHasNotification: notificationToggle,
+        habitNotificationFrequency: dayNotification,
+        habitNotificationTime: timeNotification,
+        lastCheck: formatDate,
+        daysWithoutChecks: 0,
+        habitIsChecked: 0,
+        progressBar: 1,
+      }).then(() => {
+        Alert.alert("Sucesso na criação do hábito!");
+
+        navigation.navigate("Home", {
+          createdHabit: `Created in ${habit?.habitArea}`,
+        });
       });
     }
   }
